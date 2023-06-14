7fc1911c1857933ada3dcba88f31798ec19cd0f1
vumanhtu <tu.vumanh@apuscorp.com>
2023-06-14 08:54:31 +0700

    refactor bottom navigation bar

--- lib/common/const/icon_constant.dart
+++ lib/common/const/icon_constant.dart
@@ -1,5 +1,6 @@
 class IconConst {
   static const String _path = 'assets/icons/';
+  static const String _pathimg = 'assets/images/';
   static const String back = '${_path}back.png';
   static const String logo = '${_path}logo.png';
   static const String password = '${_path}password.png';
@@ -7,11 +8,13 @@ class IconConst {
   static const String scan = '${_path}scan.png';
   static const String fakeStar = '${_path}fake_star.png';
   static const String imagePlaceHolder = '${_path}imagePlaceHolder.png';
-  static const String ca_nhan = '${_path}ca_nhan.png';
-  static const String home = '${_path}home.png';
-  static const String lich_su_quet = '${_path}lich_su_quet.png';
-  static const String tin_tuc = '${_path}tin_tuc.png';
   static const String policy_background = '${_path}policy_background.png';
   static const String iconScanHome = '${_path}icon_scan_home.png';
   static const String backGroupBottomBar = '${_path}backGroupBottomBar.png';
+
+  //new
+  static const String home = '${_pathimg}Home.png';
+  static const String lich_su_quet = '${_pathimg}Clock.png';
+  static const String tin_tuc = '${_pathimg}Paper.png';
+  static const String ca_nhan = '${_pathimg}User.png';
 }
