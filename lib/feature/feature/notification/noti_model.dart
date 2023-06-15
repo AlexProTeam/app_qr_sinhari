f57bb9d7cfa3a83f3893b6b9ad7bc677a15b6108
vumanhtu <tu.vumanh@apuscorp.com>
2023-06-15 14:32:07 +0700

    refactor notification page finally

--- lib/feature/feature/notification/noti_model.dart
+++ lib/feature/feature/notification/noti_model.dart
@@ -17,17 +17,17 @@ class NotiModel {
     image = json['image'];
     imageThumbs = json['image_thumbs'];
     createdAt = json['created_at'];
-    if (createdAt?.isNotEmpty ?? false) {
-      DateFormat dateFormat = DateFormat('yyyy-MM-ddThh:mm:ss');
-      DateTime dateTime = dateFormat.parse(createdAt!);
-      DateTime date2 = DateTime.fromMillisecondsSinceEpoch(
-          dateTime.millisecondsSinceEpoch + 3600000 * 7);
-      // LOG.d('millisecondsSinceEpochNe: $date2');
-      List<String>  ngayThang = date2.toString().substring(0,10).split('-').toList();
-      String ngayThangText = ngayThang.reversed.join('/');
-      String gio = date2.toString().substring(11,16);
-      createdAt = '$gio - $ngayThangText';
-    }
+    // if (createdAt?.isNotEmpty ?? false) {
+    //   DateFormat dateFormat = DateFormat('yyyy-MM-ddThh:mm:ss');
+    //   DateTime dateTime = dateFormat.parse(createdAt!);
+    //   DateTime date2 = DateTime.fromMillisecondsSinceEpoch(
+    //       dateTime.millisecondsSinceEpoch + 3600000 * 7);
+    //   // LOG.d('millisecondsSinceEpochNe: $date2');
+    //   List<String>  ngayThang = date2.toString().substring(0,10).split('-').toList();
+    //   String ngayThangText = ngayThang.reversed.join('/');
+    //   String gio = date2.toString().substring(11,16);
+    //   createdAt = '$gio - $ngayThangText';
+    // }
   }
 
 }
