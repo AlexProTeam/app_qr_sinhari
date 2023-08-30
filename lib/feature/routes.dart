722ed50178b1447662c529ef3d6c89a41a1c42ba
vumanhtu <tu.vumanh@apuscorp.com>
2023-08-30 09:49:48 +0700

    Refactor notification update bloc and render list noti

--- lib/feature/routes.dart
+++ lib/feature/routes.dart
@@ -16,7 +16,7 @@ import 'package:qrcode/feature/feature/home/home_screen.dart';
 import 'package:qrcode/feature/feature/list_product/list_product_screen.dart';
 import 'package:qrcode/feature/feature/news/details_news/ui/detail_new_screen.dart';
 import 'package:qrcode/feature/feature/news/news_screen/ui/news_screen.dart';
-import 'package:qrcode/feature/feature/notification/notification_screen.dart';
+import 'package:qrcode/feature/feature/notification/ui/notification_screen.dart';
 import 'package:qrcode/feature/feature/personal/gioi_thieu.dart';
 import 'package:qrcode/feature/feature/personal/huong_dan.dart';
 import 'package:qrcode/feature/feature/personal/personal_screen.dart';
