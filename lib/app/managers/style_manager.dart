aeeb41f406cdf2818774cdbc604204128ae30059
AlexAangCoder <94129871+ALexDangCoder@users.noreply.github.com>
2023-09-14 20:20:55 +0700

    fix textStyte

--- lib/app/managers/style_manager.dart
+++ lib/app/managers/style_manager.dart
@@ -3,202 +3,202 @@ import 'package:flutter/material.dart';
 import 'color_manager.dart';
 
 class TextStyleManager {
-  static get style10pxGrey => const TextStyle(
-        fontSize: 10.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.grey7,
-      );
+  static TextStyle style10pxGrey = const TextStyle(
+    fontSize: 10.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey7,
+  );
 
-  static get style12pxGrey => const TextStyle(
-        fontSize: 12.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.grey7,
-      );
+  static TextStyle style12pxGrey = const TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey7,
+  );
 
-  static get style12pxGrey6 => const TextStyle(
-        fontSize: 12.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.grey7,
-      );
+  static TextStyle style12pxGrey6 = const TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey7,
+  );
 
-  static get style12pxGrey8 => const TextStyle(
-        fontSize: 12.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.grey8,
-      );
+  static TextStyle style12pxGrey8 = const TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey8,
+  );
 
-  static get style10pxPrimary => const TextStyle(
-        fontSize: 10.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.primaryColor,
-      );
+  static TextStyle style10pxPrimary = const TextStyle(
+    fontSize: 10.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryColor,
+  );
 
-  static get normalGrey5 => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.grey5,
-      );
+  static TextStyle normalGrey5 = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey5,
+  );
 
-  static get normalGrey6 => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.grey6,
-      );
+  static TextStyle normalGrey6 = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey6,
+  );
 
-  static get normalGrey => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.grey7,
-      );
+  static TextStyle normalGrey = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey7,
+  );
 
-  static get normalPink => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.pink,
-      );
+  static TextStyle normalPink = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.pink,
+  );
 
-  static get normalGreen => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.green,
-      );
+  static TextStyle normalGreen = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.green,
+  );
 
-  static get normalYellow => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.yellow,
-      );
+  static TextStyle normalYellow = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.yellow,
+  );
 
-  static get normalBlue => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.blue,
-      );
+  static TextStyle normalBlue = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.blue,
+  );
 
-  static get normalWhite => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.white,
-      );
+  static TextStyle normalWhite = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.white,
+  );
 
-  static get normalPrimary => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.primaryColor,
-      );
+  static TextStyle normalPrimary = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryColor,
+  );
 
-  static get normalGrey8 => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.grey8,
-      );
+  static TextStyle normalGrey8 = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey8,
+  );
 
-  static get normalGrey9 => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.grey9,
-      );
+  static TextStyle normalGrey9 = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey9,
+  );
 
-  static get normalThinGrey9 => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w300,
-        color: AppColors.grey9,
-      );
+  static TextStyle normalThinGrey9 = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w300,
+    color: AppColors.grey9,
+  );
 
-  static get normalBlack => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.black,
-      );
+  static TextStyle normalBlack = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.black,
+  );
 
-  static get title => const TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.w500,
-        color: AppColors.grey9,
-      );
+  static TextStyle title = const TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w500,
+    color: AppColors.grey9,
+  );
 
-  static get bigTitle => const TextStyle(
-        fontSize: 38.0,
-        fontWeight: FontWeight.w500,
-        color: AppColors.grey9,
-      );
+  static TextStyle bigTitle = const TextStyle(
+    fontSize: 38.0,
+    fontWeight: FontWeight.w500,
+    color: AppColors.grey9,
+  );
 
-  static get medium => const TextStyle(
-        fontSize: 16.0,
-        fontWeight: FontWeight.w500,
-        color: Colors.white,
-      );
+  static TextStyle medium = const TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
 
-  static get medium20PxBlack => const TextStyle(
-        fontSize: 20.0,
-        fontWeight: FontWeight.w600,
-        color: AppColors.grey9,
-      );
+  static TextStyle medium20PxBlack = const TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w600,
+    color: AppColors.grey9,
+  );
 
-  static get mediumBlack => const TextStyle(
-        fontSize: 16.0,
-        fontWeight: FontWeight.w500,
-        color: AppColors.grey9,
-      );
+  static TextStyle mediumBlack = const TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+    color: AppColors.grey9,
+  );
 
-  static get mediumBlack14px => const TextStyle(
-        fontSize: 14.0,
-        fontWeight: FontWeight.w500,
-        color: AppColors.grey9,
-      );
+  static TextStyle mediumBlack14px = const TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w500,
+    color: AppColors.grey9,
+  );
 
-  static get mediumPink => const TextStyle(
-        fontSize: 16.0,
-        fontWeight: FontWeight.w500,
-        color: AppColors.pink,
-      );
+  static TextStyle mediumPink = const TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+    color: AppColors.pink,
+  );
 
-  static get mediumPrimary => const TextStyle(
-        fontSize: 16.0,
-        fontWeight: FontWeight.w500,
-        color: AppColors.primaryColor,
-      );
+  static TextStyle mediumPrimary = const TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+    color: AppColors.primaryColor,
+  );
 
-  static get smallGrey => const TextStyle(
-        fontSize: 12.0,
-        fontWeight: FontWeight.w400,
-        color: AppColors.grey7,
-      );
+  static TextStyle smallGrey = const TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey7,
+  );
 
-  static get styleDesProduct => const TextStyle(
-        color: Colors.black,
-        fontSize: 16,
-        fontWeight: FontWeight.w300,
-        height: 1.5,
-        wordSpacing: 1,
-        fontFamily: 'Roboto',
-      );
+  static TextStyle styleDesProduct = const TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.w300,
+    height: 1.5,
+    wordSpacing: 1,
+    fontFamily: 'Roboto',
+  );
 
-  static get normalRoboto => const TextStyle(
-        color: Colors.black,
-        fontSize: 14,
-        fontWeight: FontWeight.w400,
-        fontFamily: 'Roboto',
-      );
+  static TextStyle normalRoboto = const TextStyle(
+    color: Colors.black,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+  );
 
-  static get normalRobotoBlack => const TextStyle(
-        color: Colors.black,
-        fontSize: 14,
-        fontWeight: FontWeight.w400,
-        fontFamily: 'Roboto',
-      );
+  static TextStyle normalRobotoBlack = const TextStyle(
+    color: Colors.black,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+  );
 
-  static get normalRobotoRed => const TextStyle(
-        color: Colors.red,
-        fontSize: 14,
-        fontWeight: FontWeight.w400,
-        fontFamily: 'Roboto',
-      );
+  static TextStyle normalRobotoRed = const TextStyle(
+    color: Colors.red,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+  );
 
-  static get normalRobotoWhite => const TextStyle(
-        color: Colors.white,
-        fontSize: 14,
-        fontWeight: FontWeight.w400,
-        fontFamily: 'Roboto',
-      );
+  static TextStyle normalRobotoWhite = const TextStyle(
+    color: Colors.white,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+  );
 }
