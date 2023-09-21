0dfa3b4551effafd1ebece53fbd00a2252d4cabd
AlexAangCoder <94129871+ALexDangCoder@users.noreply.github.com>
2023-09-21 20:28:39 +0700

    fix: QA - Calculate wrong price when apply score

--- lib/presentation/feature/detail_product/product_contact/ui/detail_product_contact.dart
+++ lib/presentation/feature/detail_product/product_contact/ui/detail_product_contact.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:qrcode/app/managers/const/status_bloc.dart';
 import 'package:qrcode/presentation/feature/profile/bloc/profile_bloc.dart';
 
-import '../../../../../app/route/validate_utils.dart';
-import '../../../../../domain/entity/profile_model.dart';
-import '../../../../widgets/custom_button.dart';
-import '../../../../widgets/custom_scaffold.dart';
-import '../../../../widgets/custom_textfield.dart';
-import '../../../../widgets/dialog_manager_custom.dart';
-import '../../../../widgets/toast_manager.dart';
-import '../bloc/details_product_bloc.dart';
+import '../../../app/di/injection.dart';
+import '../../../app/route/validate_utils.dart';
+import '../../../domain/entity/profile_model.dart';
+import '../../widgets/custom_button.dart';
+import '../../widgets/custom_scaffold.dart';
+import '../../widgets/custom_textfield.dart';
+import '../../widgets/dialog_manager_custom.dart';
+import '../../widgets/toast_manager.dart';
 
 class ArgumentContactScreen {
   final int? productId;
@@ -47,82 +46,44 @@ class DetailProductContactState extends State<DetailProductContact> {
         title: 'Mua hàng',
         isShowBack: true,
       ),
-      body: BlocProvider(
-        create: (context) => DetailsProductBloc(),
-        child: BlocConsumer<DetailsProductBloc, DetailsProductState>(
-          listener: (context, state) async {
-            switch (state.status) {
-              case BlocStatusEnum.init:
-                DialogManager.hideLoadingDialog;
-                break;
-              case BlocStatusEnum.loading:
-                DialogManager.showLoadingDialog(context);
-                break;
-              case BlocStatusEnum.success:
-                await ToastManager.showToast(
-                  context,
-                  text: 'Mua hàng thành công',
-                  afterShowToast: () => Navigator.pop(context),
-                );
-                break;
-              case BlocStatusEnum.failed:
-                await ToastManager.showToast(
-                  context,
-                  text: 'Mua hàng thất bại',
-                  afterShowToast: () => Navigator.pop(context),
-                );
-                break;
-            }
-          },
-          builder: (context, state) {
-            return Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Form(
-                key: _formKey,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 20),
-                      CustomTextField(
-                        hintText: 'Họ và tên',
-                        controller: _nameController,
-                        validator: ValidateUtil.validEmpty,
-                      ),
-                      const SizedBox(height: 16),
-                      CustomTextField(
-                        hintText: 'Số điện thoại',
-                        validator: ValidateUtil.validPhone,
-                        controller: _phoneController,
-                        keyboardType: TextInputType.phone,
-                      ),
-                      const SizedBox(height: 16),
-                      CustomTextField(
-                        hintText: 'Địa chỉ',
-                        controller: _addressController,
-                        validator: ValidateUtil.validEmpty,
-                      ),
-                      const SizedBox(height: 16),
-                      CustomTextField(
-                        hintText: 'Nội dung',
-                        controller: _contentController,
-                        validator: ValidateUtil.validEmpty,
-                      ),
-                      const SizedBox(height: 30),
-                      CustomButton(
-                          width: 150,
-                          text: 'Lưu lại',
-                          onTap: () {
-                            context.read<DetailsProductBloc>().add(
-                                OnClickBuyEvent(widget.argument?.productId ?? 0,
-                                    context, _contentController, _formKey));
-                          })
-                    ],
-                  ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                CustomTextField(
+                  hintText: 'Họ và tên',
+                  controller: _nameController,
+                  validator: ValidateUtil.validEmpty,
                 ),
-              ),
-            );
-          },
+                const SizedBox(height: 16),
+                CustomTextField(
+                  hintText: 'Số điện thoại',
+                  validator: ValidateUtil.validPhone,
+                  controller: _phoneController,
+                  keyboardType: TextInputType.phone,
+                ),
+                const SizedBox(height: 16),
+                CustomTextField(
+                  hintText: 'Địa chỉ',
+                  controller: _addressController,
+                  validator: ValidateUtil.validEmpty,
+                ),
+                const SizedBox(height: 16),
+                CustomTextField(
+                  hintText: 'Nội dung',
+                  controller: _contentController,
+                  validator: ValidateUtil.validEmpty,
+                ),
+                const SizedBox(height: 30),
+                CustomButton(width: 150, text: 'Lưu lại', onTap: _onDone)
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -134,5 +95,32 @@ class DetailProductContactState extends State<DetailProductContact> {
     _phoneController.text = profileModel?.phone ?? '';
     _addressController.text = profileModel?.address ?? '';
     _contentController.text = "Tôi muốn mua hàng";
+  }
+
+  void _onDone() async {
+    try {
+      if (!_formKey.currentState!.validate()) return;
+      await DialogManager.showLoadingDialog(context);
+
+      await getIt<AppClient>().post(
+          'save-contact?product_id=${widget.argument?.productId}&content=${_contentController.text}&type=0');
+
+      if (mounted) {
+        await ToastManager.showToast(
+          context,
+          text: 'Mua hàng thành công',
+          afterShowToast: () => Navigator.pop(context),
+        );
+      }
+    } catch (e) {
+      if (mounted) {
+        await ToastManager.showToast(
+          context,
+          text: 'Mua hàng thất bại',
+          afterShowToast: () => Navigator.pop(context),
+        );
+      }
+    }
+    DialogManager.hideLoadingDialog;
   }
 }
